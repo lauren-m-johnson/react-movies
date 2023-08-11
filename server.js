6f5c0ed815e2ac734c911d2f98d7572bc842fb73
lauren-m-johnson <laurenmj90@gmail.com>
2023-08-11 16:07:32 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+require('dotenv').config();
+require('./config/database'); // Make sure this sets up the database connection
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+
+// Put API routes here, before the "catch all" route
+// Example:
+// app.use('/api', require('./routes/api'));
+
+// Configure both serve-favicon & static middleware
+// to serve from the production 'dist' folder
+app.use(favicon(path.join(__dirname, 'dist', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// The following "catch all" route (note the *) is necessary
+// to return the index.html on all non-AJAX requests
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+// Configure to use the port defined in environment variable or 3001
+const port = process.env.PORT || 3001;
+
+app.listen(port, function () {
+    console.log(`Express app running on port ${port}`);
+});
